054d20bd98545a23ca93e97542b3f7285404e088
hh <2022793246@qq.com>
2023-05-05 20:57:36 +0800

    使用swagger

--- src/main/java/com/example/LuckyApplication.java
+++ src/main/java/com/example/LuckyApplication.java
@@ -15,5 +15,4 @@ public class LuckyApplication {
     public static void main(String[] args) {
         SpringApplication.run(LuckyApplication.class, args);
     }
-
 }
--- src/main/java/com/example/filter/LoginCheckFilter.java
+++ src/main/java/com/example/filter/LoginCheckFilter.java
@@ -31,7 +31,12 @@ public class LoginCheckFilter implements Filter {
                 "/backend/**",
                 "/front/**",
                 "/user/sendMsg",
-                "/user/login"
+                "/user/login",
+
+                "/doc.html",
+                "/webjars/**",
+                "/swagger-resources",
+                "/v2/api-docs"
 
         };
         //2.判断这次请求是否需要处理
